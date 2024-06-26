431b41f907502fdb456bd8a8b41d2b3addf2ddb4
0Hoxy <tinyfinger@naver.com>
2024-06-26 11:28:36 +0900

    fixed: homepage img change

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -9,14 +9,14 @@ const HomePage = () => {
         title='아이폰 14 프로 그 이상'
         subtitle='Experience the power of the latest iPhone 14 with our most Pro camera ever.'
         link='/product/667b6e740e8be951a1625416'
-        image={`${config.backendURL}/products/iphone14pro-1.jpg`}
+        image={`${config.backendURL}/products/iphone-14-pro.webp`}
       />
       <FeaturedProducts />
       <HeroSection
         title='궁극의 장비를 세팅하세요'
         subtitle='You can add Studio Display and colour-matched Magic accessories to your bag after configure your Mac mini.'
         link='/product/667b6e740e8be951a162541e'
-        image={`${config.backendURL}/products/macbook-pro-m1-1.jpg`}
+        image={`${config.backendURL}/products/mac-system-cut.jfif`}
       />
     </div>
   );
