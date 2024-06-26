7f9ffe17f1e9753b358aa68b15cac77a66a258c4
0Hoxy <tinyfinger@naver.com>
2024-06-26 11:08:31 +0900

    fixed: homepage

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -2,6 +2,7 @@ import HeroSection from './HeroSection';
 import iphone from '../../assets/iphone-14-pro.webp';
 import mac from '../../assets/mac-system-cut.jfif';
 import FeaturedProducts from './FeaturedProducts';
+import config from '../../config.json';
 
 const HomePage = () => {
   return (
@@ -9,8 +10,8 @@ const HomePage = () => {
       <HeroSection
         title='아이폰 14 프로 그 이상'
         subtitle='Experience the power of the latest iPhone 14 with our most Pro camera ever.'
-        link='/product/667a1394e337475b06faffca'
-        image={iphone}
+        link='/product/667b6e740e8be951a1625416'
+        image={`${config.backendURL}/products/iphone-14-pro.webp`}
       />
 
       <FeaturedProducts />
@@ -18,8 +19,8 @@ const HomePage = () => {
       <HeroSection
         title='궁극의 장비를 세팅하세요'
         subtitle='You can add Studio Display and colour-matched Magic accessories to your bag after configure your Mac mini.'
-        link='/product/667a1394e337475b06faffd2'
-        image={mac}
+        link='/product/667b6e740e8be951a162541e'
+        image={`${config.backendURL}/products/mac-system-cut.jfif`}
       />
     </div>
   );
